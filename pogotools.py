c2031fda355f65fb4e84975f45e9dbfba6eac192
Nelson Yeung <nelsyeung@icloud.com>
2016-07-31 21:48:35 +0100

    Add geopy, logging and print help when no args
    - geopy has been added to allow setting the location via name. Both
      latitude and longitude, and name based location is now allowed.
    
    - Log messages have been added to show what the program is doing at
      certain points.
    
    - When the program is supplied with no arguments, print the help message
      and exit.

--- pogotools.py
+++ pogotools.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+import sys
 import os
 import argparse
 import json
 import pprint
+import logging
+import geopy
 import pgoapi
 
 
@@ -16,6 +19,7 @@ def get_iv(pokemon):
 def get_pokemon(args, res):
     """List all Pokemon with their CP and IV."""
     # Read Pokemon names from file
+    logging.info('Getting a list of all your Pokemon')
     pokemon_file = os.path.join('data', 'pokemon.json')
     with open(pokemon_file, 'r') as f:
         pokemon = json.load(f)
@@ -47,17 +51,19 @@ def get_pokemon(args, res):
             print('{:>12}   CP: {:4d}   IV: {:.2f}'.format(
                 p['name'], p['cp'], p['iv']))
 
+    logging.info('Finished listing all your Pokemon')
 
-def parser():
-    """Return parsed command line arguments."""
+
+def setup_parser():
+    """Return argparse parser."""
     parser = argparse.ArgumentParser(
         description=('Useful Pokemon Go Tools.'),
         formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument(
-        '-a', '--account', type=int, default=0,
+        '-a', '--account', metavar='index', type=int, default=0,
         help='The account index you want to use from the config.json file.\n'
-        'For example "-a 0" corresponds to the first account. (Default: "0")')
+        'For example "-a 0" corresponds to the first account. (default: "0")')
 
     parser.add_argument(
         '--get-all', action='store_true',
@@ -70,37 +76,71 @@ def parser():
     parser.add_argument(
         '-s', '--sort-by', choices=['name', 'cp', 'iv'], default='cp',
         help='Sort Pokemon by either their real name, CP or IV. '
-        '(default "CP")')
+        '(default: "CP")')
 
     parser.add_argument(
-        '--hide-cp-below', type=int, default=0,
+        '--hide-cp-below', metavar='CP', type=int, default=0,
         help='Hide pokemon below a certain CP')
 
     parser.add_argument(
-        '--show-cp-below', type=int, default=9999,
+        '--show-cp-below', metavar='CP', type=int, default=9999,
         help='Only show pokemon below a certain CP')
 
     parser.add_argument(
-        '--hide-iv-below', type=float, default=0.0,
+        '--hide-iv-below', metavar='IV', type=float, default=0.0,
         help='Hide pokemon below a certain IV')
 
     parser.add_argument(
-        '--show-iv-below', type=float, default=100.0,
+        '--show-iv-below', metavar='IV', type=float, default=100.0,
         help='Only show pokemon below a certain IV')
 
-    return parser.parse_args()
+    return parser
 
 
 def main():
-    args = parser()
+    parser = setup_parser()
+
+    # Print help message and exit if no arguments are passed in
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    # Setup logging
+    logging.basicConfig(
+        format='[%(asctime)s] %(levelname)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+    logging.getLogger('requests').setLevel(logging.WARNING)
+    logging.getLogger('pgoapi').setLevel(logging.WARNING)
 
     # Read user config file
     with open('config.json', 'r') as f:
         config = json.load(f)['accounts'][args.account]
 
+    # Check if location is latitude and longitude, or a name
+    location = config['location'].split(',')
+    if len(location) == 2:
+        try:
+            latitude = float(location[0])
+            longitude = float(location[1])
+            logging.info('Location supplied is already in latitude and'
+                         'longitude form')
+        except ValueError:
+            logging.info('Getting the longitude and latitude for %s',
+                         config['location'])
+            geolocator = geopy.geocoders.Nominatim()
+            location = geolocator.geocode(config['location'])
+            latitude = location.latitude
+            longitude = location.longitude
+
     api = pgoapi.PGoApi()
-    api.set_position(config['latitude'], config['longitude'], 0.0)
+    logging.info('Setting the position to %f, %f', latitude, longitude)
+    api.set_position(latitude, longitude, 0.0)
+    logging.info('Logging into %s', config['username'])
     api.login(config['auth_service'], config['username'], config['password'])
+    logging.info('Logged into %s successfully', config['username'])
+    logging.info('Getting user data')
     req = api.create_request()
     req.get_player()
     req.get_inventory()
